0ec8906b74f2cdf4f49b4ceda58552aa7caad932
John Asatryan <john.asatryan@gmail.com>
2021-02-06 20:53:54 +0400

    Odd elements of array

--- odd_elements_of_array.cpp
+++ odd_elements_of_array.cpp
@@ -14,14 +14,28 @@ void PrintArr(int* arr, size_t size)
 {
     for(int i = 0; i < size; ++i)
     {
-        std::cout << arr[i] << " "
+        std::cout << arr[i] << " ";
     }
     std::cout << std::endl;
 }
 
-void DeleteArr(int* &arr, size_t size)
+int OddArray(int* arr, size_t size)
 {
-    
+    int count_of_odds = 0;
+    for(int i = 0; i < size; ++i)
+    {
+        if(1 == (arr[i] & 1))
+        {
+            ++count_of_odds;
+        }
+    }
+    return count_of_odds;
+}
+
+void DeleteArr(int* arr)
+{
+    delete [] arr;
+    arr = nullptr;
 }
 
 int main()
@@ -31,5 +45,8 @@ int main()
     std::cin >> size;
     int* arr = DinamicArrFill(size);
     PrintArr(arr, size);
+    int odd_count = OddArray(arr, size);
+    std::cout << "Count of odd numbers is: " << odd_count << std::endl;
+    DeleteArr(arr);
     return 0;
 }
